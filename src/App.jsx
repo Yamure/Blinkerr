89eab9db08d0d3b6f7020d5e832a7cfbc1ffbcbe
YamukelwaTech <josephmsimango@icloud.com>
2024-12-15 20:09:53 +0200

    tailwind fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 function App() {
-  return <></>;
+  return (
+    <div className="bg-red-700">
+      <div className="bg-orange-500">hello</div>
+      hello
+    </div>
+  );
 }
 
 export default App;
