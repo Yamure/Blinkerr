6700c05391a1fba811003f4676abea0ecd3ee4c6
YamukelwaTech <josephmsimango@icloud.com>
2024-12-21 09:12:23 +0200

    improvments

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
 import { createRoot } from "react-dom/client";
-import "@/styles/index.css";
+import "@/index.css";
 import AppRoutes from "@/routes.jsx";
 
 createRoot(document.getElementById("root")).render(
