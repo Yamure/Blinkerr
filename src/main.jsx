c1211ceb6af711ee8926d8da90885dda105b06a8
YamukelwaTech <josephmsimango@icloud.com>
2025-03-14 23:37:35 +0200

    Refactor routing structure by integrating routes directly in main component; remove separate routes file for improved clarity

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,27 @@
 import '@/index.css';
-import AppRoutes from '@/routes.jsx';
+import * as Pages from '@pages';
 import { store } from '@store/store';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+const routes = [
+  { path: '/', element: <Pages.Home /> },
+  { path: '/collections', element: <Pages.Collections /> },
+  { path: '/about', element: <Pages.About /> },
+];
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <AppRoutes />
+      <Router>
+        <Routes>
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </Router>
     </Provider>
   </StrictMode>,
 );
